42d58ebda7b2c97480cafea0893dfbdc4d1ef9a9
xingchen-66-02 <xingchen-66-02@users.noreply.github.com>
2026-08-05 16:20:11 +0800

    Publish Inspire Canvas web 1.2.7 build 10

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'inspire-canvas-web-v1.2.6-build9';
+const CACHE_NAME = 'inspire-canvas-web-v1.2.7-build10';
 const SHELL = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
